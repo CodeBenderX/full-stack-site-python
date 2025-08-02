0002d039937ac5d72ee3144831b6039315391761
Angelo <angelo.tiquio@outlook.com>
2025-08-02 11:11:35 -0400

    Add navigation bar and create about and contact pages

--- server.py
+++ server.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('index.html', first_name='Julius')
+    return render_template('index.html')
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
