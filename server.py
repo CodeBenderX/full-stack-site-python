b3f17b13b1f19eb7260aba922593ec3dde5e3a3c
Angelo <angelo.tiquio@outlook.com>
2025-08-02 11:01:15 -0400

    Add Flask route and update index.html template structure

--- server.py
+++ server.py
@@ -2,5 +2,9 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def home_page():
+    return render_template('index.html', first_name='Julius')
+
 if __name__ == '__main__':
     app.run(debug=True)
